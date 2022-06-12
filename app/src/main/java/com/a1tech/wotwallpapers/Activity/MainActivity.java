c752239f6d0f297635615368672a613fbe681cd8
dckrOff <azamatotaboev7621@gmail.com>
2022-06-12 23:38:36 +0500

    added custom tab layout

--- app/src/main/java/com/a1tech/wotwallpapers/Activity/MainActivity.java
+++ app/src/main/java/com/a1tech/wotwallpapers/Activity/MainActivity.java
@@ -6,8 +6,12 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.a1tech.wotwallpapers.Adapter.TanksAdapter;
 import com.a1tech.wotwallpapers.R;
@@ -26,12 +30,20 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Tanks> tanks = new ArrayList<Tanks>();
     private TanksAdapter tanksAdapter;
 
+    private ColorStateList def;
+    private TextView item1, item2, select;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
 
+        init();
+        connectToServer();
+        onClick();
+    }
+
+    private void connectToServer() {
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -68,5 +80,39 @@ public class MainActivity extends AppCompatActivity {
     private void init() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         myRef = database.getReference("wallpapers");
+
+        item1 = findViewById(R.id.item1);
+        item2 = findViewById(R.id.item2);
+
+        select = findViewById(R.id.select);
+        def = item2.getTextColors();
+    }
+
+    private void onClick() {
+        item1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeTabs(view);
+            }
+        });
+        item2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeTabs(view);
+            }
+        });
+    }
+
+    private void changeTabs(View view) {
+        if (view.getId() == R.id.item1) {
+            select.animate().x(0).setDuration(100);
+            item1.setTextColor(Color.WHITE);
+            item2.setTextColor(def);
+        } else if (view.getId() == R.id.item2) {
+            item1.setTextColor(def);
+            item2.setTextColor(Color.WHITE);
+            int size = item2.getWidth();
+            select.animate().x(size).setDuration(100);
+        }
     }
 }
